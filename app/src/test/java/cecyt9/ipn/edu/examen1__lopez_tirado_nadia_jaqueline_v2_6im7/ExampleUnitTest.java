f8855afed01f61f11306a66ef26f3341047e1505
NadiaJaquelineLopezTirado <nadia18_lopez@hotmail.com>
2017-03-30 20:01:31 -0600

    Initial commit

---
+++ app/src/test/java/cecyt9/ipn/edu/examen1__lopez_tirado_nadia_jaqueline_v2_6im7/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package cecyt9.ipn.edu.examen1__lopez_tirado_nadia_jaqueline_v2_6im7;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
